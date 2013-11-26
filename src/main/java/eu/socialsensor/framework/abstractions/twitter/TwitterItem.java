4ed35ae097fc7e5c913916bed1143f52080dc3ac
manosetro <manosetro@iti.gr>
2013-11-26 11:17:32 +0200

    Change TwitterItem class

--- src/main/java/eu/socialsensor/framework/abstractions/twitter/TwitterItem.java
+++ src/main/java/eu/socialsensor/framework/abstractions/twitter/TwitterItem.java
@@ -76,147 +76,149 @@ public class TwitterItem extends Item {
 		}
 		else {
 			operation = Operation.NEW;
-			//Title of the tweet
-			title = status.getText();
-			//Tags 
-			HashtagEntity[] hashtags = status.getHashtagEntities();
-			tags = null;
-			if (hashtags != null) {
-				tags = new String[hashtags.length];
-				for (int i=0;i<tags.length;i++){
-					tags[i] = hashtags[i].getText();
-				}
-			}
-			//User that are mentioned inside the tweet
-			UserMentionEntity[] userMentions = status.getUserMentionEntities();
-			List<String> mentions = new ArrayList<String>();
-			for(UserMentionEntity userMention : userMentions) {
-				String screenname = userMention.getScreenName();
-				mentions.add(screenname);
-			}
-			super.mentions = mentions.toArray(new String[mentions.size()]);
-			super.inReply = status.getInReplyToScreenName();
-			
-			//Popularity
-			likes = (long) status.getFavoriteCount();
-			shares = (long) status.getRetweetCount();
-			
-			//Location
-			GeoLocation geoLocation = status.getGeoLocation();
-			if (geoLocation != null) {
-				double latitude = status.getGeoLocation().getLatitude();
-				double longitude = status.getGeoLocation().getLongitude();
-			
-				location = new Location(latitude, longitude);
-			}
-			Place place = status.getPlace();
-			if (place != null) { 
-				String placeName = status.getPlace().getFullName();
-				if(location==null) {
-					location = new Location(placeName);
-				}
-				else {
-					location.setName(placeName);
-				}
-			}
-			//WebPages inside the tweet
-			URLEntity[] urlEntities = status.getURLEntities();
-			Set<URL> urls = new HashSet<URL>();
-			webPages = new ArrayList<WebPage>();
-		
-			if (urlEntities != null) {
-				for (URLEntity urlEntity : urlEntities) {
-				
-					String urlStr = urlEntity.getExpandedURL();
-					if (urlStr == null) {
-						urlStr = urlEntity.getURL();
-						if (urlStr == null) {
-							urlStr = urlEntity.getDisplayURL();
-						}
-					}
-				
-					if(urlStr == null)
-						continue;
-				
-					try {
-						URL url = new URL(urlStr);
-						urls.add(url);
-					
-						WebPage webPage = new WebPage(urlStr, id);
-						webPage.setStreamId(streamId);
-						webPage.setDate(new Date(publicationTime));
-					
-						webPages.add(webPage);
-					} catch (Exception e) {
-						continue;
-					}
-				
-				
-				}
-			}
-			links = urls.toArray(new URL[urls.size()]);
-			
-			//MediaItems inside the tweet
-			MediaEntity[] mediaEntities = status.getMediaEntities();
-			if (mediaEntities != null) {
-				for (MediaEntity mediaEntity : mediaEntities) {
-					String mediaUrl = mediaEntity.getMediaURL();
-					if (mediaUrl == null) {
-						mediaUrl = mediaEntity.getMediaURLHttps();
-					}
-					URL temp_url;
-					try {
-						temp_url = new URL(mediaUrl);
-					} catch (MalformedURLException e) {
-						continue;
-					}
-				
-					String pageUrl = mediaEntity.getExpandedURL();
-					if(pageUrl == null) {
-						pageUrl = mediaEntity.getURL();
-					}
-				
-					//url
-					MediaItem mediaItem = new MediaItem(temp_url);
-					
-					String mediaId = SocialNetworkSource.Twitter + "#" + mediaEntity.getId();
-					
-					//id
-					mediaItem.setId(mediaId);
-					//SocialNetwork Name
-					mediaItem.setStreamId(streamId);
-					//Reference
-					mediaItem.setRef(id);
-					//Type 
-					mediaItem.setType("image");
-					//Time of publication
-					mediaItem.setPublicationTime(publicationTime);
-					//PageUrl
-					mediaItem.setPageUrl(pageUrl);
-					//Thumbnail
-					String thumbnail = mediaUrl+":thumb";
-					mediaItem.setThumbnail(thumbnail);
-					//Title
-					mediaItem.setTitle(title);
-					//Tags
-					mediaItem.setTags(tags);
-					//Popularity
-					mediaItem.setShares(shares);
-					//Location
-					mediaItem.setLocation(location);
-					//Size
-					Map<Integer, Size> sizes = mediaEntity.getSizes();
-					Size size = sizes.get(Size.MEDIUM);
-					if(size != null) {
-						mediaItem.setSize(size.getWidth(), size.getHeight());
-					}
-
-					mediaItems.add(mediaItem);
-					mediaIds.add(mediaId);
-				}
-			}
-	
 		}
+		
+		//Title of the tweet
+		title = status.getText();
+		
+		//Tags 
+		HashtagEntity[] hashtags = status.getHashtagEntities();
+		tags = null;
+		if (hashtags != null) {
+			tags = new String[hashtags.length];
+			for (int i=0;i<tags.length;i++){
+				tags[i] = hashtags[i].getText();
+			}
+		}
+		//User that are mentioned inside the tweet
+		UserMentionEntity[] userMentions = status.getUserMentionEntities();
+		List<String> mentions = new ArrayList<String>();
+		for(UserMentionEntity userMention : userMentions) {
+			String screenname = userMention.getScreenName();
+			mentions.add(screenname);
+		}
+		super.mentions = mentions.toArray(new String[mentions.size()]);
+		super.inReply = status.getInReplyToScreenName();
+			
+		//Popularity
+		likes = (long) status.getFavoriteCount();
+		shares = (long) status.getRetweetCount();
+			
+		//Location
+		GeoLocation geoLocation = status.getGeoLocation();
+		if (geoLocation != null) {
+			double latitude = status.getGeoLocation().getLatitude();
+			double longitude = status.getGeoLocation().getLongitude();
+			
+			location = new Location(latitude, longitude);
+		}
+		Place place = status.getPlace();
+		if (place != null) { 
+			String placeName = status.getPlace().getFullName();
+			if(location==null) {
+				location = new Location(placeName);
+			}
+			else {
+				location.setName(placeName);
+			}
+		}
+		
+		//WebPages inside the tweet
+		URLEntity[] urlEntities = status.getURLEntities();
+		Set<URL> urls = new HashSet<URL>();
+		webPages = new ArrayList<WebPage>();
+		
+		if (urlEntities != null) {
+			for (URLEntity urlEntity : urlEntities) {
+				
+				String urlStr = urlEntity.getExpandedURL();
+				if (urlStr == null) {
+					urlStr = urlEntity.getURL();
+					if (urlStr == null) {
+						urlStr = urlEntity.getDisplayURL();
+					}
+				}
+				
+				if(urlStr == null)
+					continue;
+				
+				try {
+					URL url = new URL(urlStr);
+					urls.add(url);
+					
+					WebPage webPage = new WebPage(urlStr, id);
+					webPage.setStreamId(streamId);
+					webPage.setDate(new Date(publicationTime));
+					
+					webPages.add(webPage);
+				} catch (Exception e) {
+					continue;
+				}	
+				
+			}
+		}
+		links = urls.toArray(new URL[urls.size()]);
+			
+		//MediaItems inside the tweet
+		MediaEntity[] mediaEntities = status.getMediaEntities();
+		if (mediaEntities != null) {
+			for (MediaEntity mediaEntity : mediaEntities) {
+				String mediaUrl = mediaEntity.getMediaURL();
+				if (mediaUrl == null) {
+					mediaUrl = mediaEntity.getMediaURLHttps();
+				}
+				URL temp_url;
+				try {
+					temp_url = new URL(mediaUrl);
+				} catch (MalformedURLException e) {
+					continue;
+				}
+				
+				String pageUrl = mediaEntity.getExpandedURL();
+				if(pageUrl == null) {
+					pageUrl = mediaEntity.getURL();
+				}
+				
+				//url
+				MediaItem mediaItem = new MediaItem(temp_url);
+					
+				String mediaId = SocialNetworkSource.Twitter + "#" + mediaEntity.getId();
+					
+				//id
+				mediaItem.setId(mediaId);
+				//SocialNetwork Name
+				mediaItem.setStreamId(streamId);
+				//Reference
+				mediaItem.setRef(id);
+				//Type 
+				mediaItem.setType("image");
+				//Time of publication
+				mediaItem.setPublicationTime(publicationTime);
+				//PageUrl
+				mediaItem.setPageUrl(pageUrl);
+				//Thumbnail
+				String thumbnail = mediaUrl+":thumb";
+				mediaItem.setThumbnail(thumbnail);
+				//Title
+				mediaItem.setTitle(title);
+				//Tags
+				mediaItem.setTags(tags);
+				//Popularity
+				mediaItem.setShares(shares);
+				//Location
+				mediaItem.setLocation(location);
+				//Size
+				Map<Integer, Size> sizes = mediaEntity.getSizes();
+				Size size = sizes.get(Size.MEDIUM);
+				if(size != null) {
+					mediaItem.setSize(size.getWidth(), size.getHeight());
+				}
+
+				mediaItems.add(mediaItem);
+				mediaIds.add(mediaId);
+			}
+		}
+	
 	}
 	
 	public TwitterItem(Status status,Feed itemFeed){
