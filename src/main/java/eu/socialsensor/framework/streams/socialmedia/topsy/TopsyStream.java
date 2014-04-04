b91933c20153bacdff478bc67899b95fabda6ce4
matzika <katerina.iliakopoulou@gmail.com>
2014-04-04 11:40:01 +0300

    add topsy stream

---
+++ src/main/java/eu/socialsensor/framework/streams/socialmedia/topsy/TopsyStream.java
@@ -0,0 +1,38 @@
+package eu.socialsensor.framework.streams.socialmedia.topsy;
+
+import org.apache.log4j.Logger;
+
+import eu.socialsensor.framework.common.domain.SocialNetworkSource;
+import eu.socialsensor.framework.retrievers.socialmedia.topsy.TopsyRetriever;
+import eu.socialsensor.framework.streams.Stream;
+import eu.socialsensor.framework.streams.StreamConfiguration;
+import eu.socialsensor.framework.streams.StreamException;
+
+
+public class TopsyStream extends Stream{
+	private Logger logger = Logger.getLogger(TopsyStream.class);
+	
+	public static final SocialNetworkSource SOURCE = SocialNetworkSource.Topsy;
+	
+	private String key;
+	
+	@Override
+	public void open(StreamConfiguration config) throws StreamException {
+		logger.info("#Topsy : Open stream");
+		
+		if (config == null) {
+			logger.error("#Topsy : Config file is null.");
+			return;
+		}
+		
+		key = config.getParameter(KEY);
+		
+		if (key == null) {
+			logger.error("#Topsy : Stream requires authentication.");
+			throw new StreamException("Stream requires authentication.");
+		}
+		
+		retriever = new TopsyRetriever(key,this);
+		
+	}
+}
